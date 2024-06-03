a8f61fd640c1778e4d30357a10f131dd1a922464
meluxmeme <meluxmeme@gmail.com>
2024-06-03 14:19:46 -0600

    use array indexing to search particular data

--- school_data.py
+++ school_data.py
@@ -18,10 +18,26 @@ from given_data import year_2013, year_2014, year_2015, year_2016, year_2017, ye
 
 def main():
     print("ENSF 692 School Enrollment Statistics")
-
+    
+   # print(data)
     # Print Stage 1 requirements here
+    array1 = np.stack((year_2013, year_2014, year_2015, year_2016, year_2017, year_2018, year_2019, year_2020, year_2021, year_2022))
+    reshapedArray = array1.reshape(10,20,3)
 
+    CenSchool_grad_10 = reshapedArray[0:10, 0, 0]
+    print(CenSchool_grad_10)
+    print(CenSchool_grad_10.mean())
+
+    CenSchool_grad_11 = reshapedArray[0:10, 0, 1]
+    print(CenSchool_grad_11)
+    print(CenSchool_grad_11.mean())
+    
+    CenSchool_grad_12 = reshapedArray[0:10, 0, 2]
+    print(CenSchool_grad_12)
+    print(CenSchool_grad_12.mean())
+    
     # Prompt for user input
+    #userData =input("Either enter school name or school code")
 
     # Print Stage 2 requirements here
     print("\n***Requested School Statistics***\n")
